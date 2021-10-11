d88b17c0302102008cb51faf07fe56716df76f2c
Sagor31h2 <sagor31h2@gmail.com>
2021-10-11 16:24:30 +0600

    Add first Property

--- Crown-angular/src/app/app.module.ts
+++ Crown-angular/src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { propertyCardComponent } from "./property/property-card/property-card.component";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    propertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
