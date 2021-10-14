389b6cd8bd9799392d7c2725ffcc618508abb9bf
Sagor31h2 <sagor31h2@gmail.com>
2021-10-14 22:05:57 +0600

    Add housing service

--- Crown-angular/src/app/app.module.ts
+++ Crown-angular/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { propertyCardComponent } from "./property/property-card/property-card.component";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HousingService } from "./services/housing.service";
+
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     propertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
-   ],
-  imports: [
-    BrowserModule
+    NavBarComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    HttpClientModule
+
+  ],
+  providers: [HousingService],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
