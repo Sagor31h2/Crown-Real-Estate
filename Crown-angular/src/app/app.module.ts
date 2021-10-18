3fb928befe217b6dc67baa46c95dbf5a24d91b60
Sagor31h2 <sagor31h2@gmail.com>
2021-10-18 21:26:13 +0600

    Add routing

--- Crown-angular/src/app/app.module.ts
+++ Crown-angular/src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { AddPropertyComponent } from "./property/add-property/add-property.component";
 import { propertyCardComponent } from "./property/property-card/property-card.component";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from "./services/housing.service";
+
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent }
+];
+
 
 
 @NgModule({
@@ -14,11 +22,13 @@ import { HousingService } from "./services/housing.service";
     AppComponent,
     propertyCardComponent,
     PropertyListComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
 
   ],
   providers: [HousingService],
